df3d3d2cb9c5676d52d83fbba09d2ca575376bde
Bartosz Wojtkowiak <b.wojtkowiak@cctechnology.pl>
2015-11-27 22:14:51 +0100

    update to react-intl v2.0.0-pr3

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Exposes a globally available `ReactIntl` object.
 
     meteor add omega:react-intl
     
+## React Intl version 2.0
+
+This wraps the new `2.0` version. If you want to use the old `1.x` version, use the `0.0.1` version of this package.
--- package.js
+++ package.js
@@ -1,23 +1,28 @@
 Package.describe({
   name: 'omega:react-intl',
-  version: '0.0.1',
+  version: '1.0.0',
   summary: 'Just a wrapper for react-intl.',
   git: 'https://github.com/wojtkowiak/meteor-react-intl',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'react-intl': '1.2.0',
+  'react-intl': '2.0.0-pr-3',
   'externalify': '0.1.0',
-  'intl': '1.0.0'
+  'intl': '1.0.1'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
-  api.use(['cosmos:browserify@0.5.1'], 'client');
+  api.use('react-runtime', 'client');
 
-  api.addFiles(['react-intl.browserify.js', 'react-intl.browserify.options.json'], 'client');
+  api.use('cosmos:browserify@0.9.2', 'client');
+
+  api.addFiles([
+    'react-intl.browserify.js',
+    'react-intl.browserify.options.json'
+  ], 'client');
 
   api.export('ReactIntl', 'client');
 });
